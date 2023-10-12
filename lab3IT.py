1bce6abc27362ebc109da57e2f024410a566b8d8
MarkMytulynskyi <mmara2003@gmail.com>
2023-10-12 20:46:11 +0300

    Initial commit

--- lab3IT.py
+++ lab3IT.py
@@ -2,13 +2,13 @@ import unittest
 from name_function import formatted_name
 
 
-class NamesTestCase(unittest.TestCase):
+def test_first_last_name():
+   result = formatted_name("mark", "mytulynskyi", "ivanovych")
+   pib = "Mark Ivanovych Mytulynskyi"
+   assert pib in result
 
-   def test_first_last_name(self):
-      result = formatted_name("mark", "mytulynskyi", "ivanovych")
-      self.assertEqual(result, "Mark Ivanovych Mytulynskyi")
 
-
-   def test_first_last_middle_name(self):
-      result = formatted_name("oleg", "vynnyk", "anatoliyovych")
-      self.assertEqual(result, "Oleg Anatoliyovych Vynnyk")
+def test_first_last_middle_name():
+   result = formatted_name("oleg", "vynnyk", "anatoliyovych")
+   pib = "Oleg Anatoliyovych Vynnyk"
+   assert pib in result
