5ae8ed2a3b66771a4c64d027a3f553e0cb9156fe
MarkMytulynskyi <mmara2003@gmail.com>
2023-10-16 16:34:42 +0300

    defend

--- test_lab3IT.py
+++ test_lab3IT.py
@@ -6,7 +6,7 @@ def test_first_last_name():
    result = formatted_name("mark", "mytulynskyi", "ivanovych")
    pib = "Mark Ivanovych Mytulynskyi"
    assert pib in result
-
+#132132
 
 def test_first_last_middle_name():
    result = formatted_name("oleg", "vynnyk", "anatoliyovych")
